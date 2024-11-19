1247cb5943c022af3bfd303c286df6f3a5b1768b
issabelth <izabela.ewa.ganko@gmail.com>
2024-11-19 12:51:44 +0100

    just relocate method

--- GitManager/Forms/IssuesForm.cs
+++ GitManager/Forms/IssuesForm.cs
@@ -26,56 +26,6 @@ namespace GitManager.Forms
             SetExampleTextLookGood();
         }
 
-        private void SetExampleTextLookGood()
-        {
-            #region example of the files
-
-            this.ExampleRichTextBox.Clear();
-
-            string hostText = "Host: ";
-            string tokenText = "Token: ";
-            string ownerText = "Owner: ";
-            string repoText = "Repo: ";
-
-            // Append the Host text in bold
-            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Bold);
-            this.ExampleRichTextBox.AppendText(hostText);
-            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Regular);
-            this.ExampleRichTextBox.SelectionStart = this.ExampleRichTextBox.TextLength;
-            this.ExampleRichTextBox.SelectionLength = 0;
-            this.ExampleRichTextBox.AppendText("<one of the following: Github/Gitlab/Bitbucket>");
-            this.ExampleRichTextBox.AppendText(Environment.NewLine);
-
-            // Append the Token text in bold
-            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Bold);
-            this.ExampleRichTextBox.AppendText(tokenText);
-            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Regular);
-            this.ExampleRichTextBox.SelectionStart = this.ExampleRichTextBox.TextLength;
-            this.ExampleRichTextBox.SelectionLength = 0;
-            this.ExampleRichTextBox.AppendText("<your security token>");
-            this.ExampleRichTextBox.AppendText(Environment.NewLine);
-
-            // Append the Owner text in bold
-            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Bold);
-            this.ExampleRichTextBox.AppendText(ownerText);
-            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Regular);
-            this.ExampleRichTextBox.SelectionStart = this.ExampleRichTextBox.TextLength;
-            this.ExampleRichTextBox.SelectionLength = 0;
-            this.ExampleRichTextBox.AppendText("<repository owner's name (needed for GitHub)>");
-            this.ExampleRichTextBox.AppendText(Environment.NewLine);
-
-            // Append the Repo text in bold
-            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Bold);
-            this.ExampleRichTextBox.AppendText(repoText);
-            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Regular);
-            this.ExampleRichTextBox.SelectionStart = this.ExampleRichTextBox.TextLength;
-            this.ExampleRichTextBox.SelectionLength = 0;
-            this.ExampleRichTextBox.AppendText("<repository/project name>");
-            this.ExampleRichTextBox.AppendText(Environment.NewLine);
-
-            #endregion example of the files
-        }
-
         /// <summary>
         /// Set the columns and their names
         /// </summary>
@@ -271,6 +221,56 @@ namespace GitManager.Forms
             {
                 this.LoadDataButton.Enabled = false;
             }
+        }
+
+        private void SetExampleTextLookGood()
+        {
+            #region example of the files
+
+            this.ExampleRichTextBox.Clear();
+
+            string hostText = "Host: ";
+            string tokenText = "Token: ";
+            string ownerText = "Owner: ";
+            string repoText = "Repo: ";
+
+            // Append the Host text in bold
+            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Bold);
+            this.ExampleRichTextBox.AppendText(hostText);
+            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Regular);
+            this.ExampleRichTextBox.SelectionStart = this.ExampleRichTextBox.TextLength;
+            this.ExampleRichTextBox.SelectionLength = 0;
+            this.ExampleRichTextBox.AppendText("<one of the following: Github/Gitlab/Bitbucket>");
+            this.ExampleRichTextBox.AppendText(Environment.NewLine);
+
+            // Append the Token text in bold
+            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Bold);
+            this.ExampleRichTextBox.AppendText(tokenText);
+            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Regular);
+            this.ExampleRichTextBox.SelectionStart = this.ExampleRichTextBox.TextLength;
+            this.ExampleRichTextBox.SelectionLength = 0;
+            this.ExampleRichTextBox.AppendText("<your security token>");
+            this.ExampleRichTextBox.AppendText(Environment.NewLine);
+
+            // Append the Owner text in bold
+            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Bold);
+            this.ExampleRichTextBox.AppendText(ownerText);
+            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Regular);
+            this.ExampleRichTextBox.SelectionStart = this.ExampleRichTextBox.TextLength;
+            this.ExampleRichTextBox.SelectionLength = 0;
+            this.ExampleRichTextBox.AppendText("<repository owner's name (needed for GitHub)>");
+            this.ExampleRichTextBox.AppendText(Environment.NewLine);
+
+            // Append the Repo text in bold
+            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Bold);
+            this.ExampleRichTextBox.AppendText(repoText);
+            this.ExampleRichTextBox.SelectionFont = new Font(this.ExampleRichTextBox.Font, FontStyle.Regular);
+            this.ExampleRichTextBox.SelectionStart = this.ExampleRichTextBox.TextLength;
+            this.ExampleRichTextBox.SelectionLength = 0;
+            this.ExampleRichTextBox.AppendText("<repository/project name>");
+            this.ExampleRichTextBox.AppendText(Environment.NewLine);
+
+            #endregion example of the files
         }
 
     }
